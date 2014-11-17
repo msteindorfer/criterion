08f7047ba05b38ce9c4f60dd53a4342a60c6479d
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2014-11-17 18:27:01 +0100

    Result version update.

--- resources/r/benchmarks.r
+++ resources/r/benchmarks.r
@@ -32,11 +32,11 @@ calculateMemoryFootprintOverhead <- function(requestedDataType, dataStructureOri
   ###
   # Load 32-bit and 64-bit data and combine them.
   ##
-  dss32_fileName <- "/Users/Michael/Dropbox/Research/hamt-improved-results/map-sizes-and-statistics-32bit-path-compression.csv"
+  dss32_fileName <- "/Users/Michael/Dropbox/Research/hamt-improved-results/map-sizes-and-statistics-32bit-20141117_1824.csv"
   dss32_stats <- read.csv(dss32_fileName, sep=",", header=TRUE)
   dss32_stats <- within(dss32_stats, arch <- factor(32))
   #
-  dss64_fileName <- "/Users/Michael/Dropbox/Research/hamt-improved-results/map-sizes-and-statistics-64bit-path-compression.csv"
+  dss64_fileName <- "/Users/Michael/Dropbox/Research/hamt-improved-results/map-sizes-and-statistics-64bit-20141117_1824.csv"
   dss64_stats <- read.csv(dss64_fileName, sep=",", header=TRUE)
   dss64_stats <- within(dss64_stats, arch <- factor(64))
   #
@@ -298,7 +298,7 @@ getBenchmarkMethodName__ <- function(arg) {
 getBenchmarkMethodName <- Vectorize(getBenchmarkMethodName__)
 
 
-benchmarksFileName <- "/Users/Michael/Dropbox/Research/hamt-improved-results/results.all-20141115_1826.log"
+benchmarksFileName <- "/Users/Michael/Dropbox/Research/hamt-improved-results/results.all-20141107_2248.log"
 benchmarks <- read.csv(benchmarksFileName, sep=",", header=TRUE, stringsAsFactors=FALSE)
 colnames(benchmarks) <- c("Benchmark", "Mode", "Threads", "Samples", "Score", "ScoreError", "Unit", "Param_dataType", "Param_sampleDataSelection", "Param_size", "Param_valueFactoryFactory")
 
