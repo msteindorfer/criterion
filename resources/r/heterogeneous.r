aadd458091fa6c2e61593d9efc5365a39cf7ae31
Michael Steindorfer <michael.steindorfer@cwi.nl>
2015-06-30 15:57:18 +0200

    Working on heterogeneous benchmarks.

--- resources/r/heterogeneous.r
+++ resources/r/heterogeneous.r
@@ -21,3 +21,12 @@ write.csv2(benchmark_data.c, file = result_fileName, quote = FALSE)
 
 resultNorm_fileName <- "/Users/Michael/Development/jmh-dscg-benchmarks/map-sizes-and-statistics_result.norm.csv"
 write.csv2(benchmark_data.c.norm, file = resultNorm_fileName, quote = FALSE)
+
+tmp <- data.frame(t(benchmark_data.c.norm[-1]))
+colnames(tmp) <- benchmark_data.c.norm[,1]
+
+resultNorm_fileName <- "/Users/Michael/Development/jmh-dscg-benchmarks/map-sizes-and-statistics_result.t.norm.csv"
+write.csv2(tmp, file = resultNorm_fileName, quote = FALSE)
+
+
+benchmark_data.c.norm[,1]
