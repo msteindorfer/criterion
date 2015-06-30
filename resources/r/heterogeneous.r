5ebdca85cb112bbfe1be8edd0d69098a52460f70
Michael Steindorfer <michael.steindorfer@cwi.nl>
2015-06-30 16:49:50 +0200

    Expanding R script for analysis.

--- resources/r/heterogeneous.r
+++ resources/r/heterogeneous.r
@@ -1,9 +1,10 @@
 #!/usr/bin/env Rscript
+setwd("~/Development/jmh-dscg-benchmarks/")
 
 library(reshape2)
 
-benchmark_fileName <- "/Users/Michael/Development/jmh-dscg-benchmarks/map-sizes-and-statistics.csv"
-benchmark_data <- read.csv(benchmark_fileName, sep=",", header=TRUE)
+benchmark_fileName <- "map_sizes_heterogeneous_exponential"
+benchmark_data <- read.csv(paste0(benchmark_fileName, ".csv"), sep=",", header=TRUE)
 
 benchmark_data.m <- melt(benchmark_data, id.vars=c('elementCount', 'className'), measure.vars=c('footprintInBytes'))
 benchmark_data.c <- dcast(benchmark_data.m,  className ~ elementCount, median, fill=0)
@@ -16,17 +17,14 @@ normalize <- function(x) {
 benchmark_data.c.norm <- benchmark_data.c
 benchmark_data.c.norm[-1] <- normalize(benchmark_data.c[-1]) 
 
-result_fileName <- "/Users/Michael/Development/jmh-dscg-benchmarks/map-sizes-and-statistics_result.csv"
+result_fileName <- paste0(benchmark_fileName, "_result.csv")
 write.csv2(benchmark_data.c, file = result_fileName, quote = FALSE)
 
-resultNorm_fileName <- "/Users/Michael/Development/jmh-dscg-benchmarks/map-sizes-and-statistics_result.norm.csv"
+resultNorm_fileName <- paste0(benchmark_fileName, "_result.norm.csv")
 write.csv2(benchmark_data.c.norm, file = resultNorm_fileName, quote = FALSE)
 
 tmp <- data.frame(t(benchmark_data.c.norm[-1]))
 colnames(tmp) <- benchmark_data.c.norm[,1]
 
-resultNorm_fileName <- "/Users/Michael/Development/jmh-dscg-benchmarks/map-sizes-and-statistics_result.t.norm.csv"
-write.csv2(tmp, file = resultNorm_fileName, quote = FALSE)
-
-
-benchmark_data.c.norm[,1]
+resultNormTransposed_fileName <- paste0(benchmark_fileName, "_result.norm.transposed.csv")
+write.csv2(tmp, file = resultNormTransposed_fileName, quote = FALSE)
