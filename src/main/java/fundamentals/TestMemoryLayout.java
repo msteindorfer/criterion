f670bd5870567de83d4680a47e1889d452f61c7c
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2016-03-25 18:00:12 +0100

    Adding basic memory layout test.

---
+++ src/main/java/fundamentals/TestMemoryLayout.java
@@ -0,0 +1,30 @@
+package fundamentals;
+
+import org.openjdk.jol.info.GraphLayout;
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.util.VMSupport;
+
+public class TestMemoryLayout {
+
+	public static void main(String[] args) {
+		new Container().printMe();
+
+		System.out.println(VMSupport.vmDetails());
+		System.out.println(ClassLayout.parseClass(Container.class).toPrintable());
+		System.out.println(GraphLayout.parseInstance(new Container()).toPrintable());
+	
+	}
+
+	private static class Container {
+
+		private final int[] constantContent = new int[] { 1, 2, 3, 4 };
+
+		public void printMe() {
+			for (int i = 0; i < constantContent.length; i++) {
+				System.out.println(constantContent[i]);
+			}
+		}
+
+	}
+
+}
