1d2b48dba7a73c25c92a43625cc7f34ec33cd5a9
Michael Steindorfer <michael@steindorfer.name>
2017-09-01 12:24:34 +0200

    Changing default.

--- runStrawman.sh
+++ runStrawman.sh
@@ -1,5 +1,5 @@
-export PARAM_SIZE="16,2048,1048576"
-export PARAM_VALUE_FACTORY="VF_SCALA_STRAWMAN,VF_SCALA"
+export PARAM_SIZE="8,2048,1048576"
+export PARAM_VALUE_FACTORY="VF_SCALA_STRAWMAN,VF_SCALA,VF_CHAMP_SPECIALIZED,VF_CHAMP,VF_DEXX,VF_VAVR,VF_PAGURO,VF_CLOJURE,VF_PCOLLECTIONS"
 export MEMORY_MEASURER_AGENT=`echo $(cd $(dirname ~); pwd)/$(basename ~)`/.m2/repository/com/github/msteindorfer/memory-measurer/0.1.0-SNAPSHOT/memory-measurer-0.1.0-SNAPSHOT.jar
 
 ####
@@ -7,7 +7,7 @@ export MEMORY_MEASURER_AGENT=`echo $(cd $(dirname ~); pwd)/$(basename ~)`/.m2/re
 ##################################
 
 java -jar ./target/benchmarks.jar \
-	"JmhSetBenchmarks.time(Lookup$|Insert$|Remove$|Iteration|Equals|SubsetOf|(Union|Subtract|Intersect)RealDuplicate)$" \
+	"JmhSetBenchmarks.time(Lookup$|Insert$|Remove$|Iteration|EqualsWorstCase)$" \
 	-f 1 -i 5 -wi 5 \
 	-p producer=PURE_INTEGER \
 	-p size=$PARAM_SIZE \
@@ -15,11 +15,13 @@ java -jar ./target/benchmarks.jar \
 	-rf text -rff logStrawman-Runtime-useCapsule-$1.txt \
 	-jvmArgsAppend "-Dstrawman.collection.immutable.useCapsule=$1"
 
-# # read -n1 -r -p "Press space to continue..." key
+# further benchmarks: SubsetOf|(Union|Subtract|Intersect)RealDuplicate
 
-####
-# MEASURE MEMORY FOOTPRINT 
-##################################
+# read -n1 -r -p "Press space to continue..." key
+
+###
+MEASURE MEMORY FOOTPRINT 
+#################################
 
 java -javaagent:$MEMORY_MEASURER_AGENT -jar ./target/benchmarks.jar \
 	"JmhSetBenchmarks.footprint$" \
